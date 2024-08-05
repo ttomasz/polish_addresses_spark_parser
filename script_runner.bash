d0d9f02e7caaaf9ecd1446442cdd1a322ca9fbbc
tomasz t <tomasztaras@outlook.com>
2024-08-05 21:16:00 +0200

    added status files

--- script_runner.bash
+++ script_runner.bash
@@ -9,9 +9,13 @@ rm data_zip/*.zip
 python3 process_gml.py osmpoland
 cp data_geoparquet/files/part*.parquet data_export/prg_adresy.parquet
 chmod 666 data_export/prg_adresy.parquet
+date --iso-8601=seconds > data_export/prg_adresy.txt
+chmod 666 data_export/prg_adresy.txt
 python3 process_gml.py overture
 cp data_geoparquet/files/part*.parquet data_export/poland_addresses.parquet
 chmod 666 data_export/poland_addresses.parquet
+date --iso-8601=seconds > data_export/poland_addresses.txt
+chmod 666 data_export/poland_addresses.txt
 rm data_xml/*.xml
 rm data_geoparquet/files/*.parquet
 rm data_geoparquet/files/.*.crc
